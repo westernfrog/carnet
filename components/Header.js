8288e8c8819f4df96370fe8bca7295bc783072e1
Aman Singh <haaamansingh007@gmail.com>
2022-12-28 18:10:38 +0530

    Updated

--- components/Header.js
+++ components/Header.js
@@ -4,7 +4,13 @@ export default function Header() {
       <nav className="navbar navbar-light container my-3 border-bottom text-slab shadow-sm">
         <div className="container-fluid p-0">
           <h1 className="navbar-brand h1 fs-3">Cheatsheet</h1>
-          <a className="btn ms-auto fs-5" type="submit">
+          <a
+            href="https://github.com/westernfrog"
+            target="_blank"
+            rel="noreferrer"
+            className="btn ms-auto fs-5 link-dark"
+            type="submit"
+          >
             <span className="d-flex flex-wrap justify-content-between align-items-center">
               <span className="d-none d-lg-block pe-2">Github</span>
               <i class="fa-brands fa-github fa-lg"></i>
