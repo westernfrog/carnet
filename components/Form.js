3b6fa8d5a3411fc06cc35a9c3f501dc89838c7d1
Aman Singh <haaamansingh007@gmail.com>
2022-12-29 16:20:30 +0530

    Updated

--- components/Form.js
+++ components/Form.js
@@ -33,7 +33,7 @@ export default function Form(params) {
   if (addMutation.isSuccess) location.reload();
   return (
     <>
-      <div class="col-md-4 mb-4">
+      <div class="col-md-4 mb-4 ">
         <form onSubmit={handleSubmit}>
           <div class="card text-dm border-2">
             <div class="card-header">
@@ -50,6 +50,7 @@ export default function Form(params) {
                   aria-label="Username"
                   onChange={setFormData}
                   autoComplete="off"
+                  autoFocus
                 />
               </h5>
               <p class="card-text">
--- components/Weather.js
+++ components/Weather.js
@@ -1,7 +1,7 @@
 export default function Weather() {
   return (
     <>
-      <div className="col-md-8">
+      <div className="col-md-8 ">
         <div class="card border-dark">
           <div class="card-header">
             <div className="row">
@@ -13,7 +13,7 @@ export default function Weather() {
                   Hold the <mark>cloud</mark> to show the weather of your area!
                 </p>
               </div>
-              <div className="col-1 text-end ms-auto pe-4 ps-0">
+              <div className="col-1 text-end ms-auto pe-4 ps-1">
                 <button className="btn btn-outline-warning rounded-pill p-0">
                   ⛅
                 </button>
@@ -23,40 +23,33 @@ export default function Weather() {
           <div class="card-body text-dark">
             <h5 class="card-title pb-2">☀️ How&apos;s it look out there?</h5>
             <div class="row text-center">
-              <div className="col-md-3">
+              <div className="col-md-4">
                 <div class="card border-0">
-                  <div class="card-header bg-white">Header</div>
+                  <div class="card-header bg-white">13&deg; C</div>
                   <div class="card-body text-dark">
-                    <h5 class="card-title">Dark card title</h5>
+                    <h2 class="card-title my-0">☁️</h2>
                   </div>
-                  <div class="card-footer bg-transparent">Footer</div>
+                  <div class="card-footer bg-transparent pb-0">Today</div>
                 </div>
               </div>
-              <div className="col-md-3">
+              <div className="col-md-4 d-none d-lg-block">
                 <div class="card border-0">
-                  <div class="card-header bg-white">Header</div>
+                  <div class="card-header bg-white">11&deg; C</div>
                   <div class="card-body text-dark">
-                    <h5 class="card-title">Dark card title</h5>
+                    <h2 class="card-title my-0">⛅</h2>
                   </div>
-                  <div class="card-footer bg-transparent">Footer</div>
+                  <div class="card-footer bg-transparent pb-0">Tomorrow</div>
                 </div>
               </div>
-              <div className="col-md-3">
+              <div className="col-md-4 d-none d-lg-block">
                 <div class="card border-0">
-                  <div class="card-header bg-white bg-white">Header</div>
+                  <div class="card-header bg-white bg-white">17&deg; C</div>
                   <div class="card-body text-dark">
-                    <h5 class="card-title">Dark card title</h5>
+                    <h2 class="card-title my-0">⛅</h2>
                   </div>
-                  <div class="card-footer bg-transparent">Footer</div>
-                </div>
-              </div>
-              <div className="col-md-3">
-                <div class="card border-0">
-                  <div class="card-header bg-white">Header</div>
-                  <div class="card-body text-dark">
-                    <h5 class="card-title">Dark card title</h5>
+                  <div class="card-footer bg-transparent pb-0">
+                    Day after tomorrow
                   </div>
-                  <div class="card-footer bg-transparent">Footer</div>
                 </div>
               </div>
             </div>
