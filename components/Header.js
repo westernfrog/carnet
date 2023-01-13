281fde0de5064e7ff2568443030367993c0b2c43
Aman Singh <haaamansingh007@gmail.com>
2023-01-13 12:56:42 +0530

    Updated

--- components/Header.js
+++ components/Header.js
@@ -3,30 +3,25 @@ import Link from "next/link";
 export default function Header() {
   return (
     <>
-      <div className="container my-5">
-        <div className="row">
-          <div className="col-lg-1"></div>
-          <div className="col-lg-11">
-            <ul className="nav">
-              <li className="nav-item">
-                <Link className="nav-link text-light ps-0" href="/">
-                  carnet
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link text-light" href="/">
-                  home
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link text-light" href="/chat">
-                  chat&nbsp;&nbsp;
-                  <i className="fa-solid fa-comment-dots grow"></i>
-                </Link>
-              </li>
-            </ul>
-          </div>
-        </div>
+      <div className="container mt-5 d-flex align-items-center mb-lg-5">
+        <ul className="nav mx-lg-auto">
+          <li className="nav-item me-lg-5">
+            <Link className="nav-link text-light ps-0" href="/">
+              carnet
+            </Link>
+          </li>
+          <li className="nav-item mx-4">
+            <Link className="nav-link text-light" href="/">
+              home
+            </Link>
+          </li>
+          <li className="nav-item ms-lg-5">
+            <Link className="nav-link text-light" href="/chat">
+              chat&nbsp;&nbsp;
+              <i className="fa-solid fa-comment-dots grow"></i>
+            </Link>
+          </li>
+        </ul>
       </div>
     </>
   );
--- pages/home.js
+++ pages/home.js
@@ -30,64 +30,61 @@ export default function Home() {
     <>
       <Header />
       <div className="container d-flex align-items-center justify-content-between">
-        <div className="row">
-          <div className="col-lg-4"></div>
-          <div className="col-lg-8">
-            <div className="mt-5">
-              <h1 className="display-3 text-dm fw-bold">carnet</h1>
-              <p className="text-muted text-rubik mb-0">
-                a place where you can see the&nbsp;
-                <span className="fw-bold text-muted">divine meeting</span> of
-                front-end with back-end.
-              </p>
-              <div className="mb-4">
-                <ul className="list-group list-group-horizontal">
-                  <li className="list-group-item bg-transparent ps-0 grow">
-                    <Link href="https://github.com/westernfrog">
-                      <i className="fa-brands fa-github"></i>
-                    </Link>
-                  </li>
-                  <li className="list-group-item bg-transparent grow">
-                    <Link href="https://www.linkedin.com/in/aman-singh123">
-                      <i className="fa-brands fa-linkedin-in"></i>
-                    </Link>
-                  </li>
-                  <li className="list-group-item bg-transparent grow">
-                    <Link href="https://www.discordapp.com/users/888993682018168842">
-                      <i className="fa-brands fa-discord"></i>
-                    </Link>
-                  </li>
-                  <li className="list-group-item bg-transparent grow">
-                    <Link href="mailto:haaamansingh007@gmail.com">
-                      <i className="fa-regular fa-envelope"></i>
-                    </Link>
-                  </li>
-                </ul>
-              </div>
-              <div className="text-rubik">
-                <p>
-                  <i className="fa-regular fa-clock"></i>&nbsp;&nbsp;
-                  <span className="getDate"></span>&nbsp;&nbsp;•&nbsp;&nbsp;
-                  <span className="getTime"></span>
-                </p>
-                <p>
-                  <i className="fa-solid fa-cloud-moon"></i>&nbsp;&nbsp;
-                  <span>It&apos;s currently&nbsp;</span>
-                  <span className="fw-bold">
-                    &nbsp;
-                    <span className="dtemp"></span>&nbsp;&nbsp;
-                  </span>
-                  <span>
-                    &#40;<span className="ddesc text-lowercase"></span>
-                    &#41;&nbsp;
-                  </span>
-                  <span className="fw-bold">
-                    &nbsp;in <span className="dcity"></span>.
-                  </span>
-                </p>
-              </div>
-              <Weather />
+        <div className="col-lg-4 mx-auto px-lg-4">
+          <div className="mt-5">
+            <h1 className="display-3 text-dm fw-bold">carnet</h1>
+            <p className="text-muted text-rubik mb-0">
+              a place where you can see the&nbsp;
+              <span className="fw-bold text-muted">divine meeting</span> of
+              front-end with back-end.
+            </p>
+            <div className="mb-4">
+              <ul className="list-group list-group-horizontal">
+                <li className="list-group-item bg-transparent ps-0 grow">
+                  <Link href="https://github.com/westernfrog">
+                    <i className="fa-brands fa-github"></i>
+                  </Link>
+                </li>
+                <li className="list-group-item bg-transparent grow">
+                  <Link href="https://www.linkedin.com/in/aman-singh123">
+                    <i className="fa-brands fa-linkedin-in"></i>
+                  </Link>
+                </li>
+                <li className="list-group-item bg-transparent grow">
+                  <Link href="https://www.discordapp.com/users/888993682018168842">
+                    <i className="fa-brands fa-discord"></i>
+                  </Link>
+                </li>
+                <li className="list-group-item bg-transparent grow">
+                  <Link href="mailto:haaamansingh007@gmail.com">
+                    <i className="fa-regular fa-envelope"></i>
+                  </Link>
+                </li>
+              </ul>
             </div>
+            <div className="text-rubik">
+              <p>
+                <i className="fa-regular fa-clock"></i>&nbsp;&nbsp;
+                <span className="getDate"></span>&nbsp;&nbsp;•&nbsp;&nbsp;
+                <span className="getTime"></span>
+              </p>
+              <p>
+                <i className="fa-solid fa-cloud-moon"></i>&nbsp;&nbsp;
+                <span>It&apos;s currently&nbsp;</span>
+                <span className="fw-bold">
+                  &nbsp;
+                  <span className="dtemp"></span>&nbsp;&nbsp;
+                </span>
+                <span>
+                  &#40;<span className="ddesc text-lowercase"></span>
+                  &#41;&nbsp;
+                </span>
+                <span className="fw-bold">
+                  &nbsp;in <span className="dcity"></span>.
+                </span>
+              </p>
+            </div>
+            <Weather />
           </div>
         </div>
       </div>
