4291a3b286eb392a7554785446eda2ce2c94a4ea
Aman Singh <haaamansingh007@gmail.com>
2023-01-13 13:01:26 +0530

    Updated

--- pages/chat.js
+++ pages/chat.js
@@ -7,23 +7,20 @@ export default function Chat() {
     <>
       <Header />
       <div className="container d-flex align-items-center justify-content-between">
-        <div className="row">
-          <div className="col-lg-3"></div>
-          <div className="col-lg-7">
-            <div className="mt-5">
-              <h1 className="display-3 text-dm fw-bold">
-                chat&nbsp;&nbsp;
-                <i className="fa-solid fa-comment-dots grow"></i>
-              </h1>
-              <p className="text-muted text-rubik mb-0">
-                the data from the input field goes directly to{" "}
-                <span className="fw-bold text-muted">MongoDB</span> database and
-                that data is display in a form of note.
-              </p>
-            </div>
-            <div className="text-rubik my-4">
-              <Form />
-            </div>
+        <div className="col-lg-5 mx-auto px-lg-5">
+          <div className="mt-5">
+            <h1 className="display-3 text-dm fw-bold">
+              chat&nbsp;&nbsp;
+              <i className="fa-solid fa-comment-dots grow"></i>
+            </h1>
+            <p className="text-muted text-rubik mb-0">
+              the data from the input field goes directly to{" "}
+              <span className="fw-bold text-muted">MongoDB</span> database and
+              that data is display in a form of note.
+            </p>
+          </div>
+          <div className="text-rubik my-4">
+            <Form />
           </div>
         </div>
       </div>
