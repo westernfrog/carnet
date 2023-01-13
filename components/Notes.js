430919eee57bbdc660dffc2997d7d6793b5cef30
Aman Singh <haaamansingh007@gmail.com>
2023-01-13 12:03:30 +0530

    Updated

--- components/Notes.js
+++ components/Notes.js
@@ -22,17 +22,6 @@ export default function Notes() {
           note junk<i class="fa-solid fa-arrow-down ms-2 grow"></i>
         </p>
         <div className="row">
-          <div className="col-md-3">
-            <div className="card mt-3 shadow-sm border-0">
-              <div className="card-body bg-dark">
-                <p className="card-title bg-dark">made with ❣️ using</p>
-                <p className="card-subtitle mb-2 bg-dark">
-                  next.js and mongodb.
-                </p>
-              </div>
-            </div>
-          </div>
-
           {data.reverse().map((obj, i) => (
             <Note {...obj} key={i} />
           ))}
