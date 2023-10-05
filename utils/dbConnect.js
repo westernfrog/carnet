54d6b1288cadac0e42162ea89ed2aa75abbe4368
Aman Singh <95922370+westernfrog@users.noreply.github.com>
2023-10-05 09:36:43 +0530

    Updated

--- utils/dbConnect.js
+++ utils/dbConnect.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectToDatabase = async () => {
   try {
-    await mongoose.connect(`${process.env.MONGO_URI}`);
+    await mongoose.connect(`${process.env.MONGO_URL}`);
   } catch (error) {
     console.error(`Failed to connect to database: ${error}`);
   }
