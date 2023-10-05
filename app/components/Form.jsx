a0df75e2fc4aa70116a6863959b53399a678bc40
Aman Singh <95922370+westernfrog@users.noreply.github.com>
2023-10-05 15:58:06 +0530

    Updated

--- app/components/Form.jsx
+++ app/components/Form.jsx
@@ -1,8 +1,9 @@
-"use client";
-
-import { PaperAirplaneIcon } from "@heroicons/react/24/outline";
-import { useState } from "react";
+import React, { useState } from "react";
 import { useMutation, useQuery, useQueryClient } from "react-query";
+import {
+  EllipsisHorizontalIcon,
+  PaperAirplaneIcon,
+} from "@heroicons/react/24/outline";
 
 async function postData(formData) {
   const response = await fetch("/api/postData", {
@@ -23,61 +24,69 @@ async function postData(formData) {
 async function fetchPosts() {
   const formRes = await fetch("/api/getData");
   const formData = await formRes.json();
-
   return formData.message;
 }
 
 export default function Form() {
   const [formData, setFormData] = useState("");
+  const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
-
   const queryClient = useQueryClient();
+
   const { data: posts } = useQuery("posts", fetchPosts);
   const mutation = useMutation(postData, {
     onSuccess: () => {
-      // Invalidate the "posts" query to trigger a re-fetch
       queryClient.invalidateQueries("posts");
       setFormData("");
+      setLoading(false);
+    },
+    onError: (error) => {
+      setError(error.message);
+      setLoading(false);
     },
   });
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
+
     if (formData.trim() === "") {
       setError("This field is required");
-      return;
+    } else {
+      setError("");
+      setLoading(true);
+      await mutation.mutateAsync(formData);
     }
-
-    setError("");
-    mutation.mutate(formData);
   };
 
   return (
-    <>
-      <div className="relative">
-        <div className="fixed inset-x-0 z-50 bottom-1 max-w-4xl mx-auto w-full p-6">
-          {error && <p className="text-red-500 text-xs p-1">{error}</p>}
-          <form className="border border-black/30 rounded-xl shadow-xl flex items-center backdrop-blur-xl">
-            <textarea
-              type="text"
-              name="formData"
-              value={formData}
-              onChange={(e) => setFormData(e.target.value)}
-              rows={3}
-              style={{ resize: "none" }}
-              placeholder="write anything.."
-              className="w-full bg-transparent border-0 rounded-xl text-sm focus:ring-0 placeholder:text-[#374151]"
-            />
-            <button
-              type="submit"
-              onClick={handleSubmit}
-              className="w-12 h-10 bg-emerald-100 ring-1 ring-black/30 shadow-xl flex items-center justify-center rounded-xl m-3"
-            >
+    <div className="relative">
+      <div className="fixed inset-x-0 z-50 bottom-1 max-w-4xl mx-auto w-full p-6">
+        {error && <p className="text-red-500 text-xs p-1">{error}</p>}
+        <form className="border border-black/30 rounded-xl shadow-xl flex items-center backdrop-blur-xl">
+          <textarea
+            type="text"
+            name="formData"
+            value={formData}
+            onChange={(e) => setFormData(e.target.value)}
+            rows={3}
+            style={{ resize: "none" }}
+            placeholder="Write anything.."
+            className="w-full bg-transparent border-0 rounded-xl text-sm focus:ring-0 placeholder:text-[#374151]"
+          />
+          <button
+            type="submit"
+            onClick={handleSubmit}
+            className="w-12 h-10 bg-emerald-100 ring-1 ring-black/30 shadow-xl flex items-center justify-center rounded-xl m-3"
+            disabled={loading}
+          >
+            {loading ? (
+              <EllipsisHorizontalIcon className="w-5 h-5 animate-pulse" />
+            ) : (
               <PaperAirplaneIcon className="w-5 h-5" />
-            </button>
-          </form>
-        </div>
+            )}
+          </button>
+        </form>
       </div>
-    </>
+    </div>
   );
 }
